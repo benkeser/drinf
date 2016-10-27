c8dfc53ed97e439c35719c0f8120b2ca42ebd42d
David Benkeser <benkeser@berkeley.edu>
2016-10-27 11:29:00 -0700

    updated documentation

--- R/simulationFunctions.R
+++ R/simulationFunctions.R
@@ -23,7 +23,7 @@
 #' to see how it is used. 
 #' @param r The exponent of the rate at which error goes to zero as function of n.
 #' 
-#' @return A \code{list} with predictions equal to truth + error
+#' @return A \code{list} with L0, A0, L1, A1, L2 to input into \code{drinf.tmle}.
 
 makeData <- function(n, b = 0.25, b0 = 1, ba = -0.25, setA = NULL){
     L0.1 <- runif(n, -4, 4)
